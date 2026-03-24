f1826c0bfddc5387b5b1a574ef3fe509fa687646
Rohith97411 <rj2002525@gmail.com>
2026-03-24 11:32:31 +0530

    version  3

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-print("hello world" )
-print ("hello im here")
+# Program to add two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Calculate sum
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
